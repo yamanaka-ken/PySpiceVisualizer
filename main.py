282f2ff110ffec70bc93da7c5073ff52eae5d711
yamanaka-ken <7320144@ed.tus.ac.jp>
2024-05-21 22:17:33 +0900

    change sample

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 from PyLTSpice import SimRunner
 from PyLTSpice import SpiceEditor
+from PyLTSpice.log.ltsteps import LTSpiceLogReader
+import pathlib
 
 # change place simulatior location
 simulator = r"C:\Program Files\LTC\LTspiceXVII\XVIIx64.exe"
 
 # select spice model
 LTC = SimRunner(output_folder='./output')
-LTC.create_netlist('./LTSpice/TEST.asc')
-netlist = SpiceEditor('./LTSpice/TEST.net')
+LTC.create_netlist('./LTSpice/Draft3.asc')
+netlist = SpiceEditor('./LTSpice/Draft3.net')
 netlist.set_component_value('fsig', '500')
 
 for fsig in (500,1000):
     netlist.set_component_value('fsig', fsig)
     print("frequency", fsig)
     LTC.run(netlist)
+
+for raw,log in LTC:
+    print("Raw file: %s, Log file: %s" % (raw, log))
+    # path =pathlib.Path(log)
+
 
 
 
