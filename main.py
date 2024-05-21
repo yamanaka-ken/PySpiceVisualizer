b73b11cc979cb0e5bbbb6794419daf45c2fecd74
yamanaka-ken <7320144@ed.tus.ac.jp>
2024-05-21 23:52:50 +0900

    change asc-files

--- main.py
+++ main.py
@@ -1,27 +1,45 @@
 from PyLTSpice import SimRunner
 from PyLTSpice import SpiceEditor
 from PyLTSpice.log.ltsteps import LTSpiceLogReader
-import pathlib
+from PyLTSpice import RawRead
+import matplotlib.pyplot as plt
+import numpy as np
 
 # change place simulatior location
 simulator = r"C:\Program Files\LTC\LTspiceXVII\XVIIx64.exe"
 
 # select spice model
 LTC = SimRunner(output_folder='./output')
+
 LTC.create_netlist('./LTSpice/Draft3.asc')
 netlist = SpiceEditor('./LTSpice/Draft3.net')
-netlist.set_component_value('fsig', '500')
+netlist.set_parameter('fsig', '500')
+# netlist.write_netlist("Draft3_Modified.net")
 
-for fsig in (500,1000):
-    netlist.set_component_value('fsig', fsig)
-    print("frequency", fsig)
+
+# set measure frequency points
+freq_list = [100, 200, 400]
+
+for fsig in freq_list:
+    netlist.set_parameter('fsig', fsig)
+    # netlist.write_netlist(f"{fsig}_Draft3.net")
     LTC.run(netlist)
+    print("frequency", fsig)
+
 
 for raw,log in LTC:
-    print("Raw file: %s, Log file: %s" % (raw, log))
-    # path =pathlib.Path(log)
+    # print("Raw file: %s, Log file: %s" % (raw, log))
+    LTR = RawRead(raw)
+    # print(LTR.get_trace_names())
+    steps = LTR.get_steps() 
+    time = LTR.get_trace("time")
+    Vn002 = LTR.get_trace("V(n002)")
 
 
+    for step in range(len(steps)):
+        # print(steps[step])
+        plt.plot(time.get_wave(step), Vn002.get_wave(step))
+        plt.savefig(f'./Draft3_{step}.png')
 
 
 
